1dfa48bab2d760765eec1aa1bc8a2622d5cd08f6
timmy-quinn <tquinn5301@gmail.com>
2025-07-22 23:01:10 -0400

    updated telescope and lualine settings

---
+++ plugin/lualine.lua
@@ -0,0 +1,43 @@
+-- Lualine configuration 
+vim.o.showmode = false
+local custom_gruvbox = require('lualine.themes.gruvbox')
+
+-- This switches around the configurations. 
+-- Note: if you run :source, then the colors will be further flipped
+custom_gruvbox.visual = custom_gruvbox.insert
+custom_gruvbox.insert = custom_gruvbox.replace 
+
+
+
+require('lualine').setup {
+  options = {
+    theme = custom_gruvbox,
+    section_separators = '',
+    component_separators = '',
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  tabline = {
+      lualine_c = {
+          {
+          'buffers',
+          max_length = vim.o.columns*0.7,
+          show_filename_only = true, 
+          show_modified_status=true, 
+            
+          symbols = {
+            modified = ' ●',      -- Text to show when modified
+          }, 
+        }
+      }, 
+      lualine_z = {'tabs'}
+  }
+
+}
+-- vim.api.nvim_set_hl(0, 'lualine_buffer_active', {link = 'Visual'}) 
