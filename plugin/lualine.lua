56b8c956bdde2a680be2648868e505ad363a1c7a
Timmy-Quinn <timmy.quinn@hannainst.com>
2025-08-22 18:31:12 -0400

    Updated lualine to highlight the current window in the tabline

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -30,7 +30,12 @@ require('lualine').setup {
           max_length = vim.o.columns*0.7,
           show_filename_only = true, 
           show_modified_status=true, 
-            
+          use_mode_colors=true,
+          buffers_color = {
+            -- Same values as the general color option can be used here.
+            active = {fg = '#c0c0d0', gui ='italic,bold'},     -- Color for active buffer.
+            inactive = 'lualine_c_inactive', -- Color for inactive buffer.
+          },
           symbols = {
             modified = ' ●',      -- Text to show when modified
           }, 
