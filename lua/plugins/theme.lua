458f24370bfd4d70117aeca3ac5205dd0fa91ec5
Timmy-Quinn <timmy.quinn@hannainst.com>
2025-09-05 17:14:43 -0400

    changed theme to use hard contrast

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,7 +3,7 @@ return {
         "ellisonleao/gruvbox.nvim", 
         priority = 1000,
         config =  function()
-            require("gruvbox").setup({})
+            require("gruvbox").setup({contrast = "hard"})
             vim.cmd("colorscheme gruvbox")
         end
     }
