46dbf6a554218517b5767ac9577a117e7b95f308
timmy-quinn <tquinn5301@gmail.com>
2025-09-05 21:44:58 -0400

    Fixed diagnostics and simplified navigation

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -11,6 +11,11 @@ return {
                 }
             },
         })
-        vim.diagnostic.config({virtual_text = false})
+        vim.diagnostic.config({virtual_text = false, underline = true})
+        -- If using tmux, add this to .tmux.conf
+        -- # Undercurl
+        -- set -g default-terminal "${TERM}"
+        -- set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
+        -- set -as terminal-overrides ',*:Setulc=\E[58::2::::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
     end
 }
