e10ab206a4c1cc7b15f69f7b49b7ebd88e0fbfdd
Timmy-Quinn <timmy.quinn@hannainst.com>
2025-10-22 22:41:38 -0400

    Updated to avoid error messages

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -87,12 +87,12 @@ return {
           -- Set up lspconfig.
           local capabilities = require('cmp_nvim_lsp').default_capabilities()
           -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-          require('lspconfig')['clangd'].setup {
-            capabilities = capabilities,
-            -- diagnostics = {
-            --     virtualText = false,
-            -- },
-          }
+          -- require('lspconfig')['clangd'].setup {
+          --   capabilities = capabilities,
+          --   -- diagnostics = {
+          --   --     virtualText = false,
+          --   -- },
+          -- }
         end
     }
 }
