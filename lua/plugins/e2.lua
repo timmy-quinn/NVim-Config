4592873135017575bcafff800c59007907cbfbf4
Timmy Q <tquinn5301@gmail.com>
2026-08-13 00:52:12 -0400

    e2 plugin

--- lua/plugins/e2.lua
+++ lua/plugins/e2.lua
@@ -1,5 +1,11 @@
-return {
-    dir = "~/nvim_plugins/e2.nvim",
-    opts = {name = "timmy"},
-}
+local is_linux = vim.uv.os_uname().sysname == "Linux"
+
+if not is_linux then
+    return {
+        dir = "~/nvim_plugins/e2.nvim",
+        opts = {name = "timmy"},
+    }
+else
+    return {}
+end
 
